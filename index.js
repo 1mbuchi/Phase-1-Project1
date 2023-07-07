9298c916a3eb172b0ca3a43eef25b2c7295fe4a9
1mbuchi Natasha <natashaimbuchi@gmail.com>
2023-07-08 02:38:39 +0300

    featred data fetched from json to js

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ for (var i = 0; i < addToCartButtons.length; i++)
 	addToCartButtons[i].addEventListener("click", function() {
 		alert("Order!");
 	}); //button function
+    //featured data fetched from js server 
+    var featured = [
+        {
+          name: "Garlic Bread",
+          description: "Toasted ciabatta bread with garlic butter.",
+          price: 4.99,
+        },
+        {
+           name: "Caprese Salad",
+           description: "Fresh mozzarella, tomatoes, and basil with balsamic glaze.",
+           price: 7.99,
+        },
+        {
+            name: "Fried Calamari",
+            description: "Lightly breaded and fried, served with marinara sauce.",
+            price: 9.99,
+        }];
